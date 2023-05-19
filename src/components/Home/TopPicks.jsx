59dd9ec36eaa492fb379877ed9a38379fe5ede15
designwithashik <designwithashik@gmail.com>
2023-05-19 20:39:19 +0600

    Top Pick Section Added

--- src/components/Home/TopPicks.jsx
+++ src/components/Home/TopPicks.jsx
@@ -10,7 +10,10 @@ const TopPicks = () => {
             <div className="carousel bg-accent  carousel-center max-w-full p-4 space-x-4 rounded-box">
                 {toys.slice(0,5).map(toy=><div className="carousel-item">
                     
-                    <img  src={toy.picture} className="rounded-box w-72 object-cover " />
+                    <div>
+                        <img  src={toy.picture} className="rounded-box h-[80%] w-72 object-cover" />
+                        <p className={`p-5 mt-4 text-white font-bold rounded-box ${toy.subCategory ==='Language Toys'?'bg-[#FF1276]' : toy.subCategory === 'Engineering Toys'?'bg-primary':'bg-error'}`}>{toy.name}</p>
+                    </div>
    
   </div> )}
             </div>
